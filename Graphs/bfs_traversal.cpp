cc8f0837eabbd4b2677e0ef1241fc90fe8f1d617
Geyanth <geyanth.2018@gmail.com>
2022-01-14 19:33:37 +0530

    Graph Traversal - BFS, DFS

---
+++ Graphs/bfs_traversal.cpp
@@ -0,0 +1,64 @@
+// Breadth First Search Traversal of Graph
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function Declaration
+vector<int> bfsOfGraph(int V, vector<int> adj[]);
+
+// Main Function
+int main(){
+
+    int V,E;
+    cin >> V >> E;
+
+    // Creating graph
+    vector<int> adj[V];
+    for(int i=0;i<E;i++){
+        int u,v;
+        cin >> u >> v;
+
+        // Directed Graph
+        adj[u].push_back(v);
+    }
+
+    vector<int> bfsTraversal;
+    bfsTraversal = bfsOfGraph(V,adj);    
+
+    for(auto it : bfsTraversal){
+        cout << it << " ";
+    }
+
+    return 0;
+}
+
+// Adding Adjacent nodes to the array -> Time Complexity - O(V+E), Space Complexity - O(V)
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+
+    vector<int> visitedArray(V,0);
+    vector<int> bfs;
+    
+    int i=0;
+    if(!visitedArray[i]){
+        queue<int> q;
+        q.push(i);
+        visitedArray[i] = 1;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            bfs.push_back(node);
+            
+            for(auto it : adj[node]){
+                if(!visitedArray[it]){
+                    visitedArray[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+    }
+
+    // For disconnected graph (when we have multiple components) - use for loop
+
+    return bfs;
+}
---
+++ Graphs/dfs_traversal.cpp
@@ -0,0 +1,61 @@
+// Depth First Search Traversal of Graph
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function Declaration
+vector<int> dfsOfGraph(int V, vector<int> adj[]);
+void dfsT(int node,vector<int> &vis,vector<int> adj[],vector<int> &dfs);
+
+// Main Function
+int main(){
+
+    int V,E;
+    cin >> V >> E;
+
+    vector<int> adj[V];
+    for(int i=0;i<E;i++){
+        int u,v;
+        cin >> u >> v;
+
+        // Undirected Graph
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<int> dfsTraversal;
+    dfsTraversal = dfsOfGraph(V,adj);
+
+    for(auto it : dfsTraversal){
+        cout << it << " ";
+    }
+
+    return 0;
+}
+
+// Using Recursion -> Time Complexity - O(V+E), Space Complexity - O(V)
+vector<int> dfsOfGraph(int V, vector<int> adj[]){
+    vector<int> visitedArray(V,0);
+    vector<int> dfs;
+
+    // Disconnected Graph (Contains Components)
+    for(int i=0;i<V;i++){
+        if(!visitedArray[i]){
+            dfsT(i,visitedArray,adj,dfs);
+        }
+    }
+
+    return dfs;
+}
+
+// Recursive Function
+void dfsT(int node,vector<int> &vis,vector<int> adj[],vector<int> &dfs){
+    dfs.push_back(node);
+    vis[node] = 1;
+
+    for(auto it : adj[node]){
+        if(!vis[it]){
+            dfsT(it,vis,adj,dfs);
+        }
+    }
+}   
