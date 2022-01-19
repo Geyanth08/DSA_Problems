21211b3b82c0c39f715b1af4d06dc9bdfe862c7e
Geyanth <geyanth.2018@gmail.com>
2022-01-19 19:57:07 +0530

    Minimum Spanning Tree Algorithms

---
+++ Graphs/kruksals_algorithm.cpp
@@ -0,0 +1,84 @@
+// Kruksal's Algorithm - Used to find Minimum Spanning Tree
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Node Declaration
+struct Node
+{
+    int u,v,wt;
+    
+    Node(int first,int second,int weight){
+        u = first;
+        v = second;
+        wt = weight;
+    }
+};
+
+// Function Declaration
+bool comp(Node a,Node b);
+int findParent(int u,vector<int> &par);
+void unionn(int u,int v,vector<int> &par,vector<int> &rank);
+
+// Main Function
+int main(){
+
+    int V,E;
+    cin >> V >> E;
+
+    vector<Node> edges;
+    for(int i=0;i<E;i++){
+        int u,v,wt;
+        cin >> u >> v >> wt;
+        edges.push_back(Node(u,v,wt));
+    }
+
+    // Kruksal Algorithm (Using disjoint set) -> Time Complexity - O((V+E)logV), Space Complexity - O(V)
+    sort(edges.begin(),edges.end(),comp);
+
+    vector<int> parent(V);
+    for(int i=0;i<V;i++){
+        parent[i] = i;
+    }
+
+    vector<int> rank(V,0);
+    vector<pair<int,int>> mst;
+    int cost = 0;
+
+    for(auto it : edges){
+        if(findParent(it.v,parent) != findParent(it.u,parent)){
+            cost += it.wt;
+            mst.push_back({it.u,it.v});
+            unionn(it.u,it.v,parent,rank);
+        }
+    }
+    
+    cout << cost << endl;
+
+    for(auto it : mst){
+        cout << it.first << "-" << it.second << endl;
+    }
+
+    return 0;
+}
+
+bool comp(Node a,Node b){
+    return a.wt < b.wt;
+}
+
+int findParent(int u,vector<int> &par){
+    if(u == par[u]) return u;
+    return findParent(par[u],par);
+}
+
+void unionn(int u,int v,vector<int> &par,vector<int> &rank){
+    u = findParent(u,par);
+    v = findParent(v,par);
+
+    if(rank[u] < rank[v]) par[u] = v;
+    else if(rank[v] < rank[u]) par[v] = u;
+    else {
+        par[v] = u;
+        rank[u]++;
+    }
+}
---
+++ Graphs/prims_algorithm.cpp
@@ -0,0 +1,94 @@
+// Prims Algorithm - Used to find Minimum Spanning Tree (MST)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function Declaration
+vector<int> prims1(vector<pair<int,int>> adj[],int V);
+vector<int> prims2(vector<pair<int,int>> adj[],int V);
+
+// Main Function
+int main(){
+    int V,E;
+    cin >> V >> E;
+
+    vector<pair<int,int>> adj[V];
+    for(int i=0;i<E;i++){
+        int u,v,wt;
+        cin >> u >> v >> wt;
+
+        adj[u].push_back(make_pair(v,wt));
+        adj[v].push_back(make_pair(u,wt));
+    }
+
+    vector<int> parent;
+    parent = prims2(adj,V);
+
+    for(int i=1;i<V;i++){
+        cout << i << " - " << parent[i] << "\n";
+    }
+    
+    return 0;
+}
+
+// Brute Force Approch -> Time Complexity - O(N^2), Space Complexity - O(N)
+vector<int> prims1(vector<pair<int,int>> adj[],int V){
+    vector<int> parent(V,-1);
+    vector<int> key(V,INT_MAX);
+    vector<bool> mstSet(V,false);
+
+    key[0] = 0;
+
+    for(int count=0;count < V-1;count++){
+        int mini=INT_MAX,u;
+        for(int i=0;i<V;i++){
+            if(mstSet[i] == false and key[i] < mini)
+                mini = key[i], u=i;
+        }
+
+        mstSet[u] = true;
+
+        for(auto it : adj[u]){
+            int v = it.first;
+            int wt = it.second;
+
+            if(mstSet[v] == false and wt < key[v]){
+                parent[v] = u,key[v] = wt;
+            }
+        }
+    }
+
+    return parent;
+}
+
+// Using priority queue -> Time Complexity - O((V+E)logV), Space Complexity - O(V)
+vector<int> prims2(vector<pair<int,int>> adj[],int V){
+    vector<int> parent(V,-1);
+    vector<int> key(V,INT_MAX);
+    vector<bool> mstSet(V,false);
+
+    priority_queue < pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+
+    key[0] = 0;
+    pq.push({0,0});
+
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+
+        mstSet[u] = true;
+
+        for(auto it : adj[u]){
+            int v = it.first;
+            int wt = it.second;
+
+            if(mstSet[v] == false and wt < key[v]){
+                parent[v] = u;
+                key[v] = wt;
+                pq.push({key[v],v});
+            }
+        }
+    }
+
+    return parent;
+}
