b3839014ce07ca6717a62bc228fb082dc21456c3
Geyanth <geyanth.2018@gmail.com>
2022-05-12 19:10:22 +0530

    Merge Two Lists

---
+++ Linked_list/merge_two_sorted_linked_lists.cpp
@@ -0,0 +1,50 @@
+// Leetcode Link - https://leetcode.com/problems/merge-two-sorted-lists/
+// Merge Two sorted lists into one sorted list
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// Using Merge Sort Approach -> Time Complexity - O(N+M), Space Complexity - O(1)
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+
+        if (l1 == NULL)
+            return l2;
+        if (l2 == NULL)
+            return l1;
+
+        if (l1->val > l2->val)
+            swap(l1, l2);
+
+        ListNode *head = l1;
+
+        while (l1 != NULL and l2 != NULL)
+        {
+            ListNode *temp = NULL;
+
+            while (l1 != NULL and l1->val <= l2->val)
+            {
+                temp = l1;
+                l1 = l1->next;
+            }
+
+            temp->next = l2;
+            swap(l1, l2);
+        }
+
+        return head;
+    }
+};
